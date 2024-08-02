c21a67c27c935ff1e9c8152996f8e69453b0efca
Ansh <anshsaxena4190@gmail.com>
2024-08-03 00:52:21 +0530

    feat: reactjs configuration

---
+++ reactjs/craco.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+    webpack: {
+        configure: (webpackConfig, { env, paths }) => {
+            // Modify the rules
+            webpackConfig.module.rules = webpackConfig.module.rules.map(
+                (rule) => {
+                    if (Array.isArray(rule.oneOf)) {
+                        rule.oneOf[rule.oneOf.length - 1].exclude = [
+                            /\.(js|mjs|jsx|cjs|ts|tsx)$/,
+                            /\.html$/,
+                            /\.json$/,
+                        ];
+                    }
+                    return rule;
+                }
+            );
+
+            // Add fallbacks
+            webpackConfig.resolve.fallback = {
+                ...webpackConfig.resolve.fallback,
+                buffer: require.resolve("buffer/"),
+                crypto: require.resolve("crypto-browserify"),
+                stream: require.resolve("stream-browserify"),
+                vm: require.resolve("vm-browserify"),
+                http: false,
+                https: false,
+                zlib: false,
+                url: false,
+            };
+
+            return webpackConfig;
+        },
+    },
+};
---
+++ reactjs/src/App.tsx
@@ -0,0 +1,27 @@
+import './App.css';
+import { LitNodeClient } from "@lit-protocol/lit-node-client";
+import { LitNetwork } from "@lit-protocol/constants";
+
+function App() {
+
+  async function instantiateLit() {
+    console.log("connection lit..")
+    const litNodeClient = new LitNodeClient({
+      litNetwork: LitNetwork.DatilDev,
+      debug: false
+    });
+
+    await litNodeClient.connect();
+    console.log("connected!")
+  }
+
+  return (
+    <div className="App">
+      <h1>Lit with React</h1>
+      <p>Check console</p>
+      <button onClick={instantiateLit}>Instantiate Lit</button>
+    </div>
+  );
+}
+
+export default App;
