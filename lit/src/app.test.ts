adc7cf301bc66c21892e8246fdd25c82f7d6a4b7
Ansh <anshsaxena4190@gmail.com>
2024-09-01 15:23:40 +0530

    fix: package.json and build errors

--- lit/src/app.test.ts
+++ lit/src/app.test.ts
@@ -3,20 +3,20 @@ import { fixture, html } from '@open-wc/testing';
 import '@testing-library/jest-dom';
 import './app';
 
-// describe('App', () => {
-//   it('should display "Connecting to Lit..." and then "Connected!" when the button is clicked', async () => {
-//     const el = await fixture(html`<app></app>`);
+describe('App', () => {
+  it('should display "Connecting to Lit..." and then "Connected!" when the button is clicked', async () => {
+    const el = await fixture(html`<app></app>`);
 
-//     const button = el.shadowRoot?.querySelector('button');
+    const button = el.shadowRoot?.querySelector('button');
 
-//     button?.click();
+    button?.click();
 
-//     expect(el.shadowRoot?.querySelector('div')).toHaveTextContent('Connecting to Lit...');
-//     await new Promise((resolve) => setTimeout(resolve, 5000));
-//     expect(el.shadowRoot?.querySelector('div')).toHaveTextContent('Connected!');
-//   });
-// });
-
-test('adds 1 + 2 to equal 3', () => {
-  expect(3).toBe(3);
+    expect(el.shadowRoot?.querySelector('div')).toHaveTextContent('Connecting to Lit...');
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+    expect(el.shadowRoot?.querySelector('div')).toHaveTextContent('Connected!');
+  });
 });
+
+// test('adds 1 + 2 to equal 3', () => {
+//   expect(3).toBe(3);
+// });
